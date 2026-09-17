5b65e99f50df67b086ce10474491e5c60fcfe77b
a757k <wowwowwow12355@gmail.com>
2026-09-17 21:03:41 +0300

    make BottomTabs.jsx

---
+++ src/components/BottomTabs.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+const modes = [
+  {
+    id: "BIG",
+    description: "LARGE"
+  },
+
+  {
+    id: "MEDIUM",
+    description: "ANIMALS"
+  },
+
+  {
+    id: "SMALL",
+    description: "TINY"
+  }
+];
+
+function BottomTabs({
+  mode,
+  setMode
+}) {
+  return (
+    <nav className="bottom-tabs">
+      {modes.map((item) => (
+        <button
+          key={item.id}
+          className={`tab ${
+            mode === item.id
+              ? "active"
+              : ""
+          }`}
+          onClick={() =>
+            setMode(item.id)
+          }
+        >
+          <span className="tab-name">
+            {item.id}
+          </span>
+
+          <span className="tab-description">
+            {item.description}
+          </span>
+        </button>
+      ))}
+    </nav>
+  );
+}
+
+export default BottomTabs;
